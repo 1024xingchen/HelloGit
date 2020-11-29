e32b301a908c1af9ae72f46e643eca42f6d44671
1024xingchen <15143289522@163.com>
2020-11-29 15:23:15 +0800

    更新版本一

--- HelloGit/src/main/java/com/atguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/git/HelloGit.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hellow Git!");
+
+        System.out.println("gengxin Version1");
     }
 }
